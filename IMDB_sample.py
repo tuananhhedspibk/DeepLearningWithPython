46ac7f738c9934eae4814f60e06c9eed9ba32bce
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2018-12-30 15:48:01 +0900

    Fix model fit

--- IMDB_sample.py
+++ IMDB_sample.py
@@ -20,28 +20,48 @@ y_train = np.asarray(train_labels).astype("float32")
 x_test = vectorize_sequences(test_data)
 y_test = np.asarray(test_labels).astype("float32")
 
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
 model = models.Sequential()
 model.add(layers.Dense(16, activation="relu", input_shape=(10000,)))
 model.add(layers.Dense(16, activation="relu"))
 model.add(layers.Dense(1, activation="sigmoid"))
-model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
+model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["acc"])
 
-model.fit(x_train, y_train, epochs=4, batch_size=512)
+history = model.fit(partial_x_train, partial_y_train,
+  epochs=10, batch_size=512, validation_data=(x_val, y_val))
 results = model.evaluate(x_test, y_test)
 
 y_pred = model.predict(x_test)
 
-# history_dict = history.history
-# loss_values = history_dict["loss"]
-# val_loss_values = history_dict["val_loss"]
+history_dict = history.history
+loss_values = history_dict["loss"]
+val_loss_values = history_dict["val_loss"]
 
-# epochs = range(1, 21)
+epochs = range(1, len(loss_values) + 1)
 
-# plt.plot(epochs, loss_values, "bo", label="Training Loss")
-# plt.plot(epochs, val_loss_values, "b", label="Validation Loss")
-# plt.title("Training and validation loss")
-# plt.xlabel("Epochs")
-# plt.ylabel("Loss")
-# plt.legend()
+plt.plot(epochs, loss_values, "bo", label="Training Loss")
+plt.plot(epochs, val_loss_values, "b", label="Validation Loss")
+plt.title("Training and validation loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
 
-# plt.show()
+plt.show()
+
+plt.clf() # clear figure
+acc_values = history_dict["acc"]
+val_acc_values = history_dict["val_acc"]
+
+plt.plot(epochs, acc_values, "bo", label="Training accuracy")
+plt.plot(epochs, val_acc_values, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+
+plt.show()
